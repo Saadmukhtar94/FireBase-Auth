40238eab781e928a377c0aa74ba41e150b4c697a
Saadmukhtar94 <saadmukhtar584@gmail.com>
2025-07-31 14:08:29 +0500

    register form validation

--- src/app/Register/page.js
+++ src/app/Register/page.js
@@ -1,16 +1,30 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import Link from 'next/link'
+import { register } from '@/lib/firebase/auth';
 export default function page() {
+    const [email,setEmail] = useState('');
+    const[password,setPassword] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        try {
+            await register(email,password);
+            alert('Registered Successfully');
+            }
+            catch (error){
+                alert('error.message');
+            }
+    }
   return (
-    <form className=' container d-flex justify-content-center align-items-center gap-3 flex-column border bg-warning bg-opacity-10 py-3 mt-4 w-auto'>
+    <form onClick={handleSubmit} className=' container d-flex justify-content-center align-items-center gap-3 flex-column border bg-warning bg-opacity-10 py-3 mt-4 w-auto'>
         <h1>Register</h1>
 
         <label htmlFor="email" >Email</label>
-        <input type="email" placeholder='Enter Your Email' className='w-25 form-control' required/>
+        <input type="email" placeholder='Enter Your Email' onChange={(e) => setEmail(e.target.value)} className='w-25 form-control' required/>
           <label htmlFor="password" >Set-Password</label>
-        <input type="password" placeholder='Enter Your Password' className='w-25 form-control' required/>
-        <label htmlFor="password" >Confirm Password</label>
-        <input type="password" placeholder='Enter Your Password' className='w-25 form-control' required/>
+        <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder='Enter Your Password' className='w-25 form-control' required/>
+        {/* <label htmlFor="password" >Confirm Password</label>
+        <input type="password" placeholder='Enter Your Password' className='w-25 form-control' required/> */}
  <div className='d-flex justify-content-around gap-2'> 
             <p className='fs-6'>already have an account?</p>
         <Link href="/Login" className='text-warning'>Sign-in</Link>   
